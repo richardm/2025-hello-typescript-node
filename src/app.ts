44123d6cd7fd248e3be4d6a7f9fd224f772e1eca
Richard Morgan <richard@richardmorgan.com>
2024-12-29 02:01:25 -0800

    Fix issues

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,6 @@
 import express, { Request, Response } from 'express';
 const app = express();
 
-function testImplicitAny(x) {
-  return x.toUpperCase();
-}
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
