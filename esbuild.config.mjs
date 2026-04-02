beef739b45ae79bf7dcf7a681f1b78bf3c669e0b
Richard Morgan <richard@richardmorgan.com>
2026-04-02 01:52:51 +0000

    Fix prettier formatting... for some reason Codespaces is not running prettier automatically

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,13 +1,15 @@
 import * as esbuild from 'esbuild';
 
-await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  platform: 'node',
-  target: ['node20'], // Specify your Node.js version
-  outfile: 'dist/index.js',
-  minify: process.env.NODE_ENV === 'production',
-  sourcemap: true,
-  // Recommended for Node apps: don't bundle node_modules
-  packages: 'external', 
-}).catch(() => process.exit(1));
+await esbuild
+  .build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    platform: 'node',
+    target: ['node20'], // Specify your Node.js version
+    outfile: 'dist/index.js',
+    minify: process.env.NODE_ENV === 'production',
+    sourcemap: true,
+    // Recommended for Node apps: don't bundle node_modules
+    packages: 'external',
+  })
+  .catch(() => process.exit(1));
